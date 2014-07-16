86c478ea9ff7e3e19e07cdaec48eb7368cde9261
Chris Dowling <chrisd@ralphandco.com>
2014-07-16 22:24:08 +0100

    adding frontend

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+module.exports = function (grunt) {
+
+    // Displays the elapsed execution time of grunt tasks
+    require('time-grunt')(grunt);
+
+    // Load NPM Tasks
+    require('load-grunt-tasks')(grunt, ['grunt-*', '!grunt-template-jasmine-istanbul', '!grunt-template-jasmine-requirejs']);
+
+    // Project configuration.
+    grunt.initConfig({
+
+        // Store your Package file so you can reference its specific data whenever necessary
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                mangle: false,
+                beautify: true
+            },
+            static_mappings: {
+                // Because these src-dest file mappings are manually specified, every
+                // time a new file is added or removed, the Gruntfile has to be updated.
+                files: {
+                    'assets_min/js/admin.js': [
+                        'assets/js/vendor/jquery.js',
+                        'assets/js/bootstrap/affix.js',
+                        'assets/js/bootstrap/alert.js',
+                        'assets/js/bootstrap/button.js',
+                        'assets/js/bootstrap/carousel.js',
+                        'assets/js/bootstrap/collapse.js',
+                        'assets/js/bootstrap/dropdown.js',
+                        'assets/js/bootstrap/tab.js',
+                        'assets/js/bootstrap/transition.js',
+                        'assets/js/bootstrap/scrollspy.js',
+                        'assets/js/bootstrap/modal.js',
+                        'assets/js/bootstrap/tooltip.js',
+                        'assets/js/bootstrap/popover.js',
+                        'assets/js/admin.js'],
+                    'assets_min/js/frontend.js': [
+                        'assets/js/vendor/jquery.js',
+                        'assets/js/vendor/*.js'
+                    ]
+                }
+            }
+        },
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded',
+                    compass: 'true',
+                    sourcemap: true,
+                    lineNumbers: true,
+                    require: ['bootstrap-sass']
+                },
+                files:{
+                    'assets_min/css/admin.css' : 'assets/sass/admin.scss'
+                }
+            }
+        },
+
+        // Run: `grunt watch` from command line for this section to take effect
+        watch: {
+            js : {
+                files: ['<%= jshint.files %>'],
+                tasks: ['jshint', 'uglify']
+            },
+            css : {
+                files: ['<%= sass.dist.files %>'],
+                tasks: 'sass'
+            }
+        },
+
+        concurrent: {
+          options: {
+            logConcurrentOutput: true
+          },
+          prod: {
+            tasks: ["watch:js", "watch:css"]
+          },
+          dev: {
+            tasks: ["watch:js", "watch:css"]
+          }
+        }
+
+    });
+
+    // Default Task
+    grunt.registerTask('default', ['jshint', 'uglify', 'sass']);
+
+    grunt.registerTask("dev", ["concurrent:dev"]);
+
+    // Release Task
+    grunt.registerTask('release', ['jshint', 'uglify:dist', 'sass']);
+
+    /*
+        Notes:
+
+        When registering a new Task we can also pass in any other registered Tasks.
+        e.g. grunt.registerTask('release', 'default requirejs'); // when running this task we also run the 'default' Task
+
+        We don't do this above as we would end up running `sass:dev` when we only want to run `sass:dist`
+        We could do it and `sass:dist` would run afterwards, but that means we're compiling sass twice which (although in our example quick) is extra compiling time.
+
+        To run specific sub tasks then use a colon, like so...
+        grunt sass:dev
+        grunt sass:dist
+    */
+
+};
--- ci_app/views/home.php
+++ ci_app/views/home.php
@@ -1,17 +1,231 @@
-<h1>Blog Posts</h1>
-<?php foreach($blog as $post){
-	$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'blog'));
-}?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Little Vanilla Kitchen</title>
+	<link href="<?php echo base_url(); ?>assets_min/css/styles.css" rel="stylesheet" type="text/css" media="all"/>
+	<link href="<?php echo base_url(); ?>assets_min/css/foundation-icons.min.css" rel="stylesheet" type="text/css" media="all"/>
+</head>
+<body class="wrapper">
+	<div id="superContainer animted fadeIn">
+		<div id="fullpage">
+			<nav class="nav" id="nav">
+				<ul>
+					<li data-menuanchor="about"><a href="#about">About</a></li>
+					<li data-menuanchor="gallery"><a href="#gallery">Gallery</a></li>
+					<li data-menuanchor="lvk" class="logo"><a href="#lvk"><img src="<?php echo base_url(); ?>assets_min/img/lvk-logo.png"></a></li>
+					<li data-menuanchor="news"><a href="#news">News</a></li>
+					<li data-menuanchor="contact"><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
 
-<h1>Gallery Items</h1>
-<?php foreach($gallery as $post){
-	$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'gallery'));
-}?>
+			<div class="section active" id="section0">
+				<!--div class="big-logo"><img src="public/img/lvk-logo.png"></div-->
+				<div class="intro-box">
+					<p class="strapline">Bespoke cakes for all occasions</p>
+				</div>
+				<div class="hero-image"></div>
+					<!--section class="content first">
+					   <ul class="cb-slideshow">
+			            <li><span>Image 01</span></li>
+			            <li><span>Image 02</span></li>
+			            <li><span>Image 03</span></li>
+			            <li><span>Image 01</span></li>
+			            <li><span>Image 02</span></li>
+			            <li><span>Image 03</span></li>
+			        </ul>
+			    </section-->
+			</div>
+			<div class="section" id="section1">
+				<h1 class="heading">About Little Vanilla Kitchen</h1>
+				<section class="content">
+					<h2 class="sub-heading">Little Vanilla Kitchen specialises in making delicious, beautiful cakes.</h2>
+					<section>
+						<div>
+							<p>Whether you want to surprise a loved one with some decadent cupcakes, desire a couture creation for your wedding day or need a birthday cake to remember, we have the cakes for you. Whatever your requirement, simply contact us for a friendly chat about what you need and how we can help.</p>
+							<p>At Little Vanilla Kitchen, we only source the freshest, quality ingredients to provide the most gorgeous flavoured cakes. Our motto? It may look too good to eat, but it's too delicious to resist!</p>
+							<p>Claudia Bloomfield is the cake-obsessed creator of Little Vanilla Kitchen and has been making cakes since childhood. Now, years later, after leaving behind the bright lights of London and having her own bun in the oven, Claudia balances raising a family with self-raising flour. The result is Little Vanilla Kitchen and cakes with the wow factor.</p>
+						</div>
+						<div class="cta-container"><a href="#" class="more">Read the FAQ's</a></div>
+					</section>
+				</section>
+			</div>
+			<div class="section" id="section2">
+				<h1 class="heading">Gallery</h1>
+				<section class="content">
+					<div class="sub-heading-section">
+						<h2 class="sub-heading">Sub heading for the tasty gallery</h2>
+					</div>
+					<section>
+						<div class="gallery">
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/1.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/2.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text. It could fit 3 lines at the most.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/3.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/4.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/1.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text. It could fit 3 lines at the most.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/2.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text. It could fit 3 lines at the most.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/3.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text.</p>
+							</div>
+							<div class="gallery-item">
+								<img class="gallery-image" src="<?php echo base_url(); ?>assets_min/img/4.jpg">
+								<p class="gallery-caption">Photo caption. This space currently fits around two lines of text. It could fit 3 lines at the most.</p>
+							</div>
+						</div>
+						<div class="cta-container"><a href="#" class="more">Check Out The Menu</a></div>
+					</section>
+				</section>
+			</div>
+			<div class="section" id="section3">
+				<h1 class="heading">Latest from the Blog</h1>
+				<section class="content">
+					<div class="sub-heading-section">
+						<h2>BBQ Maple Syrup Cupcakes</h2>
+						<p><i>JUNE 5TH, 2014</i><p>
+						</div>
+						<section>
+							<div>
+								<img class="blog-hero" src="<?php echo base_url(); ?>assets_min/img/hero2.jpg">
+								<p>Whether you want to surprise a loved one with some decadent cupcakes, desire a couture creation for your wedding day or need a birthday cake to remember, we have the cakes for you. Whatever your requirement, simply contact us for a friendly chat about what you need and how we can help.</p>
+								<p>At Little Vanilla Kitchen, we only source the freshest, quality ingredients to provide the most gorgeous flavoured cakes. Our motto? It may look too good to eat, but it's too delicious to resist!</p>
+								<p>Claudia Bloomfield is the cake-obsessed creator of Little Vanilla Kitchen and has been making cakes since childhood. Now, years later, after leaving behind the bright lights of London and having her own bun in the oven, Claudia balances raising a family with self-raising flour. The result is Little Vanilla Kitchen and cakes with the wow factor.</p>
+								<p>At Little Vanilla Kitchen, we only source the freshest, quality ingredients to provide the most gorgeous flavoured cakes. Our motto? It may look too good to eat, but it's too delicious to resist!</p>
+								<p>Claudia Bloomfield is the cake-obsessed creator of Little Vanilla Kitchen and has been making cakes since childhood. Now, years later, after leaving behind the bright lights of London and having her own bun in the oven, Claudia balances raising a family with self-raising flour. The result is Little Vanilla Kitchen and cakes with the wow factor.</p>
+							</div>
+							<div class="cta-container"><a href="#" class="more">Read More</a></div>
+						</section>
+					</section>
+				</div>
+				<div class="section" id="section4">
+					<h1 class="heading">Contact</h1>
+					<section class="content last">
+						<div class="sub-heading">
+							<h2>Sub heading for contact</h2>
+						</div>
+						<section>
+							<div>
+								<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam dignissim convallis est. Quisque aliquam. Donec faucibus. Nunc iaculis suscipit dui. Nam sit amet sem. Aliquam libero nisi, imperdiet at, tincidunt nec, gravida vehicula, nisl. Praesent mattis, massa quis luctus fermentum, turpis mi volutpat justo, eu volutpat enim diam eget metus.</p>
+								<fieldset>
+									<form>
+										<p><label for="text_field">Name</label>
+											<input type="text" id="text_field"></p>
+											<p><label for="text_area">Email</label>
+												<input type="text" id="text_field"></p>
+												<p><label for="text_area">Telephone (optional)</label>
+													<input type="text" id="text_field"></p>
+										<!--textarea id="text_area"></textarea></p>
+										<p><label for="select_element">Select Element:</label>
+										<select name="select_element">
+											<optgroup label="Option Group 1">
+												<option value="1">Option 1</option>
+												<option value="2">Option 2</option>
+												<option value="3">Option 3</option>
+											</optgroup>
+											<optgroup label="Option Group 2">
+												<option value="1">Option 1</option>
+												<option value="2">Option 2</option>
+												<option value="3">Option 3</option>
+											</optgroup>
+										</select></p-->
+										<p><label for="radio_buttons">Enquiry type:</label>
+											<label>
+												<input type="radio" class="radio" name="radio_button" value="radio_1"> General enquiry
+											</label>
+											<label>
+												<input type="radio" class="radio" name="radio_button" value="radio_2"> Make an order
+											</label>
+										<!--label>
+											<input type="radio" class="radio" name="radio_button" value="radio_3"> Radio 3
+										</label>
+										</p>
+										<p><label for="checkboxes">Checkboxes:</label>
+										<label>
+											<input type="checkbox" class="checkbox" name="checkboxes" value="check_1"> Radio 1
+										</label>
+										<label>
+											<input type="checkbox" class="checkbox" name="checkboxes" value="check_2"> Radio 2
+										</label>
+										<label>
+											<input type="checkbox" class="checkbox" name="checkboxes" value="check_3"> Radio 3
+										</label>
+										</p>
+										<p><label for="password">Password:</label>
+										<input type="password" class="password" name="password">
+										</p>
+										<p><label for="file">File Input:</label>
+										<input type="file" class="file" name="file">
+									</p-->
+									<div class="cta-container"><input class="button" type="submit" value="Send Enquiry"></p>
+									</form>
+								</fieldset>
+							</div>
+						</section>
+						<div class="sub-heading">
+							<h2>Find us online</h2>
+						</div>
+						<ul class="social-links">
+							<li><a href="#"><i class="fi-social-twitter"></i></a></li>
+							<li><a href="#"><i class="fi-social-facebook"></i></a></li>
+							<li><a href="#"><i class="fi-mail"></i></a></li>
+						</ul>
+					</section>
+				</div>
 
-<h1>Menu Items</h1>
-<?php foreach($menu as $post){
-	$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'menu'));
-}?>
+			</div>
+		</div>
+		<h1>Blog Posts</h1>
+		<?php foreach($blog as $post){
+			$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'blog'));
+		}?>
 
-<h1>About</h1>
-<?php $this->load->view('posts/'.$about->post_type, array('post'=>$about, 'category'=>'about')); ?>
+		<h1>Gallery Items</h1>
+		<?php foreach($gallery as $post){
+			$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'gallery'));
+		}?>
+
+		<h1>Menu Items</h1>
+		<?php foreach($menu as $post){
+			$this->load->view('posts/'.$post->post_type, array('post'=>$post, 'category'=>'menu'));
+		}?>
+
+		<h1>About</h1>
+		<?php $this->load->view('posts/'.$about->post_type, array('post'=>$about, 'category'=>'about')); ?>
+	</body>
+	<link href='http://fonts.googleapis.com/css?family=Arapey:400italic,400' rel='stylesheet' type='text/css'>
+	<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/vendor/jquery.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/vendor/jquery.easings.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/vendor/jquery.slimscroll.min.js"></script>
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/vendor/jquery.fullPage.min.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function() {
+		$('#fullpage').fullpage({
+			autoScrolling: false,
+			anchors: ['lvk', 'about', 'gallery','news','contact'],
+			animateAnchor: true,
+			easing: 'easeInOutCubic',
+			menu: '#nav',
+			scrollingSpeed: 450,
+			paddingBottom: '100px'
+		});
+	});
+	</script>
+</html>
